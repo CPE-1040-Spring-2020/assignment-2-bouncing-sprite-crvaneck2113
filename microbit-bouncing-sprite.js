76fe1f2f384095d28374cb813bfefb1f52d275a0
crvaneck2113 <54683765+crvaneck2113@users.noreply.github.com>
2020-01-27 18:56:25 -0700

    Update microbit-bouncing-sprite.js

--- microbit-bouncing-sprite.js
+++ microbit-bouncing-sprite.js
@@ -1,13 +1,16 @@
 let character = game.createSprite(2, 2)
 
+input.onButtonPressed(Button.B, () => {
+    character.set(LedSpriteProperty.X, Math.randomRange(0, 4))
+    character.set(LedSpriteProperty.Y, Math.randomRange(0, 4))
+})
+input.onButtonPressed(Button.A, () => {
+        character.turn(Direction.Right, 45)
+    
+})
 basic.forever(function () {
     character.move(1)
     character.ifOnEdgeBounce()
-    input.onButtonPressed(Button.A, () => {
-        character.turn(Direction.Right, 45)
-    })
-    input.onButtonPressed(Button.B, () => {
-        character.set(LedSpriteProperty.X, Math.randomRange(0, 4))
-        character.set(LedSpriteProperty.Y, Math.randomRange(0, 4))
-    })
 })
+
+// Moved input out of forever loop 
